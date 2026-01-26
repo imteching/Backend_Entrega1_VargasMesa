8cd29f076608baf978b362d046222b48ca9ff74d
Maria Isabel <marie.antoinette18@gmail.com>
2026-01-26 03:40:43 -0300

    ultimos detalles

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectMongoDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect("mongodb+srv://entrega:appEnsayo@cluster0.urdtny7.mongodb.net/entregaFinal?appName=Cluster0");
         console.log("✅ Conectado a MongoDB");
     } catch (error) {
         console.error("❌ Error conectando a MongoDB:", error.message);
